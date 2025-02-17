45e5248099518c918cd5931ed3eb6523c264e0f2
Никита Семенчук <n.semenchuk@texture.finance>
2025-02-17 23:53:53 +0300

    asd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'src/types/supabase.ts', 'load_collection.js'],
+    ignores: ['eslint.config.mjs', 'src/types/supabase.ts', 'load_collection.js', '.puppeteerrc.cjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
