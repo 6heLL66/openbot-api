8c4fd4b8a1d9e943e4aeade4d05b7c1e69ce291c
Никита Семенчук <n.semenchuk@texture.finance>
2025-02-17 23:41:57 +0300

    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: './.env',
     }),
